d8e55785347f555ed7fe779cb2affb3ffc2ac734
Leonardo Lima <leonardolima453@gmail.com>
2024-07-09 01:04:43 -0300

    Alterações no navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import  Logo  from "../../components/Imagens/Logo.png";
 import insta from "../../components/Imagens/icons8-instagram.svg";
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const NavBar = () => {
   return (
@@ -9,15 +10,15 @@ const NavBar = () => {
         <a href="/" className="d-flex align-items-center mb-2 mb-md-0 me-md-auto text-dark text-decoration-none">
           <img src={Logo} alt="BillyBrinquedos" width={130} height={100} />
         </a>
-        <ul className="nav nav-pills">
+        <ul className="nav nav-pills align-self-center">
           <li className="nav-item">
-          <NavLink to={"/"} className="nav-link">Inicio</NavLink>
+          <NavLink to={"/"} className="nav-link"><i className="bi bi-house"></i> Inicio</NavLink>
           </li>
           <li className="nav-item">
-          <NavLink to={"/Catalogo/"} className="nav-link">Catálogo</NavLink>
+          <NavLink to={"/Catalogo/"} className="nav-link"><i className="bi bi-book"></i> Catálogo</NavLink>
           </li>
           <li className="nav-item">
-          <NavLink to={"/Sobrenos/"} className="nav-link">Sobre Nós</NavLink>
+          <NavLink to={"/Sobrenos/"} className="nav-link"><i className="bi bi-file-earmark-person"></i> Sobre Nós</NavLink>
           </li>
           <li>
           <a target="_blank" href="https://instagrma.com/billybrinquedos"><img src={insta} alt="Instagram do BillyBrinquedos" /></a>
